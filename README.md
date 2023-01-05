7ee4fd076e6540c0bab18f01bab4c49c4133131c
Prashant Ghimire <pras.ghim@gmail.com>
2023-01-05 14:58:01 -0600

    updated readme and some code

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # YAML to Props
 
-Developed using python 3.10
-How it works?
+What is this tool about?
 
-1. Go to the directory where your application.yml file resides.
-2. Run the command as `python3 main.py`
-3. You should see an output confirming `.properties` files that were written.
+This is a python script that can be used to generate Spring Framework's `.properties` files from an existing `.yml` file.
+It will generate separate `application-{profile}.properties` for each profile.
+
+Spring uses [snakeyaml](https://bitbucket.org/snakeyaml/snakeyaml/issues/561/cve-2022-1471-vulnerability-in) which has CVE-1471 to read Yaml file. 
+If we use `.properties` files instead, we can safely exclude snakeyaml dependency.
+
+1. Clone this repo to a safe directory
+2. `cd yaml-to-properties`
+3. Install dependencies using command: `pip3 install -r requirements.txt`
+4. Place your `application.yml` file under `input` directory.
+5. Run the command as `python3 main.py`
+6. `.properties` file should be under `output` directory.
+
+### Folders after run
+![Folders](images/folder.png)
+### Snakeyaml Removal Process
+1. Run `mvn dependency:tree` and see any snakeyaml transitive dependency.
+2. Exclude snakeyaml using maven exclusion.
+3. Run `mv dependency:tree` again to ensure there's no snakeyaml
+4. Run the application to ensure its functional.
--- main.py
+++ main.py
@@ -1,38 +1,55 @@
 from collections.abc import MutableMapping
 
+import shutil
 import yaml
 
+PROFILE_DECLARATION_KEYS = ['spring.config.activate.on-profile', 'spring.profiles.active', 'spring.profiles']
 
-def flatten(d, parent_key='', sep='.') -> MutableMapping:
+
+def normalize_map(d, parent_key='', sep='.') -> MutableMapping:
     items = []
     for k, v in d.items():
         new_key = parent_key + sep + k if parent_key else k
         if isinstance(v, MutableMapping):
-            items.extend(flatten(v, new_key, sep=sep).items())
+            items.extend(normalize_map(v, new_key, sep=sep).items())
         else:
             if isinstance(v, list):
-                items.append((new_key, ', '.join(v)))
+                items.append((new_key, ','.join(v)))
             else:
                 items.append((new_key, v))
     return dict(items)
 
 
 def main():
+    # delete properties file in output folder before running
+    shutil.rmtree('./output/*.properties', ignore_errors=True)
+    print('output directory cleaned')
+
     with open("./input/application.yml", mode='rt', encoding='utf-8') as file:
-        data = yaml.safe_load_all(file)
-        count = 0
-        for d in data:
-            count += 1
-            flattened_map = flatten(d)
-            profile_prop_key = 'spring.config.activate.on-profile'
-            if profile_prop_key in flattened_map:
-                new_file_name = f'application-{flattened_map["spring.config.activate.on-profile"]}.properties'
+        profiles = yaml.safe_load_all(file)
+        for profile in profiles:
+            normalized_map = normalize_map(profile)
+
+            profile_key = None
+            for key in PROFILE_DECLARATION_KEYS:
+                if key in normalized_map:
+                    profile_key = key
+                    break
+
+            if profile_key in normalized_map:
+                if 'local' in normalized_map[profile_key]:
+                    new_file_name = f'application-local.properties'
+                else:
+                    new_file_name = f'application-{normalized_map[profile_key]}.properties'
             else:
                 new_file_name = 'application.properties'
             with open(f'./output/{new_file_name}', 'w') as new_file:
-                for k, v in flattened_map.items():
+                for k, v in normalized_map.items():
+                    # skip this prop since we don't need it.
+                    if k in PROFILE_DECLARATION_KEYS:
+                        continue
                     new_file.write(f'{k}={v}\n')
-            print(f'wrote {new_file_name}')
+            print(f'created {new_file_name}')
 
 
 if __name__ == '__main__':
